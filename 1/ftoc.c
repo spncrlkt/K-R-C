4db01aed220fd423aada2fc1e06af55f9d07972d
Spencer Liechty <spencerliechty@gmail.com>
2011-04-20 23:03:44 -0500

    First Commit

---
+++ 1/ftoc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	printf("Fahrenheit\tCelsius\n");
+	printf("==========================\n");
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%9.0f \t %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
+
